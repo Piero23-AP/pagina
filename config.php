ddc1f80d6142cda6757e9734403c7be3317214a7
JEFERSONFLO <fflorescuba905@gmail.com>
2023-08-11 18:14:40 -0500

    conversion php

--- config.php
+++ config.php
@@ -4,15 +4,15 @@ $username = "root";
 $password = "admin9538125@@asSXUl";
 $database = "DashboardAppDB";
 
-// Crear la conexión
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar la conexión
+
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
 
-// Cerrar la conexión
+
 $conn->close();
 ?>
